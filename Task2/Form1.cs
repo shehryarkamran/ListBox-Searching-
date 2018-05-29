e99e98d7c41e66d5676c186acf56ee893a41c3b8
Shehryar Kamran <kool_dude1996@live.com>
2018-05-29 14:15:54 +0500

    Initial commit

---
+++ Task2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text.Equals(""))
+            {
+                MessageBox.Show("TextBox is empty!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                listBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select value first!!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(listBox1.Items.Contains(textBox1.Text))
+            {
+                MessageBox.Show("Found!!", "congrats", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Not found!", "sorry", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
